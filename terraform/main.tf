f427eca18bc336b60d29d2725bb276134ca9f55f
Douglas Hindson <dmhindson@gmail.com>
2024-09-26 10:38:18 -0400

    Fix ci.

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,8 +157,11 @@ resource "google_secret_manager_secret" "openai_api_key" {
 }
 
 resource "google_secret_manager_secret_version" "openai_api_key_version" {
-  secret      = google_secret_manager_secret.openai_api_key.id
-  secret_data = var.openai_api_key
+  secret = google_secret_manager_secret.openai_api_key.id
+
+  payload {
+    data = var.openai_api_key
+  }
 }
 
 resource "google_secret_manager_secret" "pinecone_api_key" {
@@ -170,7 +173,10 @@ resource "google_secret_manager_secret" "pinecone_api_key" {
 }
 
 resource "google_secret_manager_secret_version" "pinecone_api_key_version" {
-  secret      = google_secret_manager_secret.pinecone_api_key.id
-  secret_data = var.pinecone_api_key
+  secret = google_secret_manager_secret.pinecone_api_key.id
+
+  payload {
+    data = var.pinecone_api_key
+  }
 }
 
